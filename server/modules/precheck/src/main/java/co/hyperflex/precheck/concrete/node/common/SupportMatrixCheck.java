9d46fff3fe59e9818a3b92526b21c598e59e13b7
vijay patidar <vramotiya@gmail.com>
2025-09-29 17:58:45 +0530

    refactor ElasticSupportMatrixCheck.java to SupportMatrixCheck to support botn type of node

--- server/modules/precheck/src/main/java/co/hyperflex/precheck/concrete/node/common/SupportMatrixCheck.java
+++ server/modules/precheck/src/main/java/co/hyperflex/precheck/concrete/node/common/SupportMatrixCheck.java
@@ -1,22 +1,24 @@
-package co.hyperflex.precheck.concrete.node.elastic;
+package co.hyperflex.precheck.concrete.node.common;
 
 import co.hyperflex.core.models.clusters.Distro;
+import co.hyperflex.core.models.enums.ClusterNodeType;
 import co.hyperflex.core.models.enums.ClusterType;
 import co.hyperflex.precheck.contexts.NodeContext;
-import co.hyperflex.precheck.core.BaseElasticNodePrecheck;
+import co.hyperflex.precheck.core.BaseNodePrecheck;
+import co.hyperflex.precheck.core.PrecheckFailedException;
 import co.hyperflex.precheck.supportmatrix.OsSupport;
 import co.hyperflex.precheck.supportmatrix.OsSupportLoaderUtils;
 import co.hyperflex.precheck.utils.VersionUtils;
 import org.springframework.stereotype.Component;
 
 /**
- * Precheck that verifies whether the target Elasticsearch version
+ * Precheck that verifies whether the target Elasticsearch/Kibana version
  * is supported on the current node's operating system according to
  * the official Elastic support matrix.
  *
  * <p>
  * - Checks both OS type and version.
- * - Checks if the target Elasticsearch version falls within a supported range.
+ * - Checks if the target Elasticsearch/Kibana version falls within a supported range.
  * - Logs informative messages and throws a RuntimeException if the version
  * or OS is unsupported.
  *
@@ -24,11 +26,11 @@ import org.springframework.stereotype.Component;
  * See: https://www.elastic.co/support/matrix
  */
 @Component
-public class ElasticSupportMatrixCheck extends BaseElasticNodePrecheck {
+public class SupportMatrixCheck extends BaseNodePrecheck {
 
   @Override
   public String getName() {
-    return "Elastic support matrix check";
+    return "Support matrix check";
   }
 
   @Override
@@ -41,39 +43,34 @@ public class ElasticSupportMatrixCheck extends BaseElasticNodePrecheck {
     var targetVersion = context.getClusterUpgradeJob().getTargetVersion();
     var distro = context.getNode().getOs().distro();
     var logger = context.getLogger();
-
-    boolean osMatched = false;
+    var node = context.getNode();
+    var nodeLabel = node.getType() == ClusterNodeType.ELASTIC ? "Elasticsearch" : "Kibana";
 
     logger.info("Please visit https://www.elastic.co/support/matrix for details.");
 
-    for (OsSupport osSupport : OsSupportLoaderUtils.loadElasticOsSupports()) {
+    for (OsSupport osSupport : OsSupportLoaderUtils.loadOsSupports(context.getNode().getType())) {
       if (isSameOs(distro, osSupport) && isSameVersion(distro, osSupport)) {
-        osMatched = true;
         for (var support : osSupport.supports()) {
           if (VersionUtils.inRange(targetVersion, support.start(), support.end())) {
             if (support.supported()) {
-              logger.info("Elasticsearch version {} is supported on current node OS [{} {}]",
-                  targetVersion, distro.name(), distro.version());
+              logger.info("{} version {} is supported on current node OS [{} {}]",
+                  nodeLabel, targetVersion, distro.name(), distro.version());
               return; // success, no need to check further
             } else {
-              logger.error("Elasticsearch version {} is NOT supported on current node OS [{} {}]",
-                  targetVersion, distro.name(), distro.version());
-              throw new RuntimeException("Elasticsearch version not supported on node OS");
+              logger.error("{} version {} is unsupported on current node OS [{} {}]",
+                  nodeLabel, targetVersion, distro.name(), distro.version());
+              throw new PrecheckFailedException();
             }
           }
         }
       }
     }
 
-    if (!osMatched) {
-      logger.error("Current node OS [{} {}] is not listed in the Elastic support matrix.",
-          distro.name(), distro.version());
-    } else {
-      logger.error("Unable to verify support for Elasticsearch version {} on current node OS [{} {}].",
-          targetVersion, distro.name(), distro.version());
-    }
+    logger.error("Unable to verify support for {} version {} on current node OS [{} {}].",
+        nodeLabel, targetVersion, distro.name(), distro.version());
 
-    throw new RuntimeException("Elasticsearch version support could not be verified");
+    logger.error("{} version support could not be verified", nodeLabel);
+    throw new PrecheckFailedException();
   }
 
   private boolean isSameVersion(Distro distro, OsSupport osSupport) {
