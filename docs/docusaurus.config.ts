f080176f478e7b873286381b0a38613faab1a57d
vijay patidar <vramotiya@gmail.com>
2025-10-17 17:31:30 +0530

    (chore) Fixed github link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-            href: 'https://github.com/langchain4j/langchain4j',
+            href: 'https://github.com/ConsultaddHQ/elastic-seamless-upgrade-tool',
             position: 'right',
             className: 'header-github-link',
             'aria-label': 'GitHub repository',
