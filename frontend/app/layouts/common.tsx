42174689d2f94405156c51b90ddda36ac4cfadbb
vijay patidar <vramotiya@gmail.com>
2025-10-30 14:19:47 +0530

    Added logout button

--- frontend/app/layouts/common.tsx
+++ frontend/app/layouts/common.tsx
@@ -1,24 +1,40 @@
 import { addToast, Button, Divider, ToastProvider, useDisclosure } from "@heroui/react"
 import { Box, Typography } from "@mui/material"
-import { Edit, ElementPlus, Magicpen, Refresh2, Setting2, TickCircle, Warning2 } from "iconsax-react"
+import { Edit, ElementPlus, LogoutCurve, Magicpen, Refresh2, Setting2, TickCircle, Warning2 } from "iconsax-react"
 import { useEffect } from "react"
 import { FiArrowUpRight, FiX } from "react-icons/fi"
 import { Link, Outlet, useLocation, useNavigate } from "react-router"
 import EditCluster from "~/components/core/EditCluster"
 import Settings from "~/components/core/Settings"
 import UpcomingFeature from "~/components/core/UpcomingFeature"
+import { useConfirmationModal } from "~/components/utilities/ConfirmationModal"
 import AssetsManager from "~/constants/AssetsManager"
 import { useRealtimeEventListener } from "~/lib/hooks/useRealtimeEventListener"
 import { cn } from "~/lib/Utils"
+import { useLocalStore } from "~/store/common"
 import { useSocketStore } from "~/store/socket"
 
 function Common() {
 	const { connect, disconnect } = useSocketStore()
 	const { isOpen, onOpen, onOpenChange } = useDisclosure()
+	const { ConfirmationModal, openConfirmation } = useConfirmationModal()
 	const { isOpen: isSettingsOpen, onOpen: onSettingsOpen, onOpenChange: onSettingsOpenChange } = useDisclosure()
 	const { isOpen: isEditOpen, onOpen: onEditOpen, onOpenChange: onEditOpenChange } = useDisclosure()
 	const { pathname } = useLocation()
 	const navigate = useNavigate()
+	const setSession = useLocalStore((state) => state.setSessionName)
+
+	const logout = () => {
+		openConfirmation({
+			title: "Logout",
+			message: "Are you sure you want to logout?",
+			Icon: ({ size }) => <LogoutCurve size={size} color={"#E87D65"} style={{ transform: "rotate(180deg)" }} />,
+			onConfirm: () => {
+				setSession("")
+				navigate("/login")
+			},
+		})
+	}
 
 	useEffect(() => {
 		connect() // Connect on mount
@@ -129,12 +145,22 @@ function Common() {
 					>
 						<Magicpen variant="Bold" color="currentColor" size="20px" /> Upcoming features
 					</Button>
+					<Button
+						aria-label="Settings"
+						variant="light"
+						radius="none"
+						className="min-w-11 min-h-11 text-[#E87D65]"
+						onPress={logout}
+					>
+						<LogoutCurve color="#E87D65" size="20px" style={{ transform: "rotate(180deg)" }} /> Logout
+					</Button>
 				</Box>
 			</Box>
 			<EditCluster isOpen={isEditOpen} onOpenChange={onEditOpenChange} />
 			<UpcomingFeature isOpen={isOpen} onOpenChange={onOpenChange} />
 			<Settings isOpen={isSettingsOpen} onOpenChange={onSettingsOpenChange} />
 			<Outlet />
+			{ConfirmationModal}
 		</Box>
 	)
 }
