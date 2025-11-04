4a26388d8ef780c37484df0f851a699d6cba7c2c
srajansohani <99042007+srajansohani@users.noreply.github.com>
2025-11-04 13:31:46 +0530

    Updated Disk usage Precheck for ECH

---
+++ server/modules/elastic-client/src/main/java/co/hyperflex/clients/elastic/dto/nodes/NodeFileSystem.java
@@ -0,0 +1,17 @@
+package co.hyperflex.clients.elastic.dto.nodes;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NodeFileSystem {
+
+    @JsonProperty("total")
+    private ExtendedMemoryStats mem;
+
+    public ExtendedMemoryStats getMem() {
+        return mem;
+    }
+
+    public void setMem(ExtendedMemoryStats mem) {
+        this.mem = mem;
+    }
+}
