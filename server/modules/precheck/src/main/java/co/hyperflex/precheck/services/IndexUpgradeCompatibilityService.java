d2139ca4974c5a8f700e62e3ea7975e51a5bc78d
Nilesh Morkar <nilesh.m@consultadd.com>
2026-01-15 03:30:50 +0530

    Optimise code for lucene check

--- server/modules/precheck/src/main/java/co/hyperflex/precheck/services/IndexUpgradeCompatibilityService.java
+++ server/modules/precheck/src/main/java/co/hyperflex/precheck/services/IndexUpgradeCompatibilityService.java
@@ -1,13 +1,10 @@
 package co.hyperflex.precheck.services;
 
-import co.hyperflex.clients.client.ApiRequest;
 import co.hyperflex.clients.elastic.ElasticsearchClientProvider;
 import co.hyperflex.core.services.upgrade.ClusterUpgradeJobService;
 import co.hyperflex.precheck.services.dtos.IndexReindexInfo;
 import co.hyperflex.precheck.utils.IndexUtils;
-import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,11 +12,13 @@ public class IndexUpgradeCompatibilityService {
 
   private final ElasticsearchClientProvider elasticsearchClientProvider;
   private final ClusterUpgradeJobService clusterUpgradeJobService;
+  private final IndexUtils indexUtils;
 
   public IndexUpgradeCompatibilityService(ElasticsearchClientProvider elasticsearchClientProvider,
-                                          ClusterUpgradeJobService clusterUpgradeJobService) {
+                                          ClusterUpgradeJobService clusterUpgradeJobService, IndexUtils indexUtils) {
     this.elasticsearchClientProvider = elasticsearchClientProvider;
     this.clusterUpgradeJobService = clusterUpgradeJobService;
+    this.indexUtils = indexUtils;
   }
 
   public List<IndexReindexInfo> getReindexIndexesMetadata(String clusterId) {
@@ -29,39 +28,12 @@ public class IndexUpgradeCompatibilityService {
     int targetLucene = IndexUtils.mapEsVersionToLucene(upgradeJob.getTargetVersion());
 
     return indices.stream()
-        .filter(indicesRecord -> !isLuceneCompatible(clusterId, indicesRecord.getIndex(), targetLucene))
+        .filter(indicesRecord -> !indexUtils.isLuceneCompatible(clusterId, indicesRecord.getIndex(), targetLucene))
         .map(indicesRecord -> new IndexReindexInfo(
             indicesRecord.getIndex(),
             indicesRecord.getDocsSize(),
             indicesRecord.getDocsCount()
         )).toList();
-  }
-
-  public boolean isLuceneCompatible(String clusterId, String indexName, int targetLucene) {
-    var request = ApiRequest.builder(JsonNode.class).get().uri("/" + indexName + "/_segments").build();
-    var root = elasticsearchClientProvider.getClient(clusterId).execute(request);
-    AtomicInteger minLuceneVersion = new AtomicInteger(Integer.MAX_VALUE);
-
-    JsonNode segmentsNode = root.path("indices").path(indexName).path("shards");
-
-    // Iterate over shards and find min Lucene version
-    segmentsNode.properties().forEach(entry -> {
-      entry.getValue().forEach(shard -> {
-        JsonNode segments = shard.path("segments");
-        segments.properties().forEach(segEntry -> {
-          JsonNode segment = segEntry.getValue();
-          String versionStr = segment.path("version").asText(); // e.g. "8.11.1"
-          if (!versionStr.isEmpty()) {
-            int major = Integer.parseInt(versionStr.split("\\.")[0]);
-            minLuceneVersion.set(Math.min(minLuceneVersion.get(), major));
-          }
-        });
-      });
-    });
-
-    //validate minimum lucene version
-    // ES supports indices created with at most one major Lucene version older => targetLucene - 1
-    return minLuceneVersion.get() == Integer.MAX_VALUE || minLuceneVersion.get() >= targetLucene - 1;
   }
 
 }
--- server/modules/precheck/src/main/java/co/hyperflex/precheck/utils/IndexUtils.java
+++ server/modules/precheck/src/main/java/co/hyperflex/precheck/utils/IndexUtils.java
@@ -1,15 +1,62 @@
 package co.hyperflex.precheck.utils;
 
+import co.hyperflex.clients.client.ApiRequest;
+import co.hyperflex.clients.elastic.ElasticsearchClientProvider;
+import co.hyperflex.core.constants.ElasticsearchApiPaths;
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class IndexUtils {
   private static final Map<String, Integer> esToLucene = Map.of("5", 6, "6", 7, "7", 8, "8", 9, "9", 10);
+  private final ElasticsearchClientProvider elasticsearchClientProvider;
 
-  public IndexUtils() {
+  public IndexUtils(ElasticsearchClientProvider elasticsearchClientProvider) {
+    this.elasticsearchClientProvider = elasticsearchClientProvider;
   }
 
   public static int mapEsVersionToLucene(String elasticVersion) {
-    String major = elasticVersion.substring(0, 1);
+    String major = elasticVersion.split("\\.")[0];
     return esToLucene.getOrDefault(major, -1);
   }
+
+  public boolean isLuceneCompatible(String clusterId, String indexName, int targetLucene) {
+    var request = ApiRequest.builder(JsonNode.class)
+        .get()
+        .uri(String.format(ElasticsearchApiPaths.SEGMENTS, indexName))
+        .build();
+    var root = elasticsearchClientProvider.getClient(clusterId).execute(request);
+
+    // ES supports indices created with at most one major Lucene version older => targetLucene - 1
+    int minAllowed = targetLucene - 1;
+
+    JsonNode shardsNode = root.path("indices").path(indexName).path("shards");
+
+    for (Map.Entry<String, JsonNode> stringJsonNodeEntry : shardsNode.properties()) {
+      JsonNode shardArray = stringJsonNodeEntry.getValue();
+
+      for (JsonNode shard : shardArray) {
+        JsonNode segments = shard.path("segments");
+
+        for (Map.Entry<String, JsonNode> jsonNodeEntry : segments.properties()) {
+          JsonNode segment = jsonNodeEntry.getValue();
+          String versionStr = segment.path("version").asText();
+
+          if (versionStr.isEmpty()) {
+            continue;
+          }
+
+          int major = Integer.parseInt(versionStr.split("\\.")[0]);
+
+          if (major < minAllowed) {
+            return false; // reindex required
+          }
+        }
+      }
+    }
+
+    return true; // compatible
+  }
+
 }
