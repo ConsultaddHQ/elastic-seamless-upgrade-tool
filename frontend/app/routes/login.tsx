615c9ac47c6bacc3369dfe58d216c14648dde0cd
Nilesh Morkar <nilesh.m@consultadd.com>
2026-02-13 23:17:30 +0530

    Updated version: Enable the enter key feature for form submit.

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -44,111 +44,107 @@ export default function LoginPage() {
 		},
 	})
 	return (
-		<Box component="form" onSubmit={formik.handleSubmit} className="w-full">
-			<Box
-				className="w-full flex items-center justify-center mt-[50px]" // centers child vertically and horizontally
-				padding={{ xs: "32px 16px", lg: "32px 56px 32px 152px" }}
-				sx={{ minHeight: "100vh" }} // ensures full viewport height for vertical centering
-			>
-				<CssBaseline />
-				<Box className="flex flex-col w-full gap-10 max-w-4xl">
-					<Box className="flex flex-col gap-4 max-w-[515px] w-full">
-						<Box
-							className="flex items-center justify-center rounded-[10px] p-px w-min"
-							sx={{
-								background:
-									"linear-gradient(135deg, #6627FF 2.29%, #C9C0DF 44.53%, #131315 97.18%, #131315 97.18%)",
-							}}
-						>
-							<Typography className="flex items-center justify-center size-10 rounded-[9px] font-manrope font-semibold text-xl leading-[22px] bg-black">
-								👋
-							</Typography>
-						</Box>
-						<Typography fontSize="24px" fontWeight={600} lineHeight="22px" color="#FFF">
-							Login to your account
-						</Typography>
-						<Typography fontSize="16px" fontWeight={500} lineHeight="22px" color="#ADADAD">
-							Welcome back! Please enter your details.
-						</Typography>
-					</Box>
+		<Box
+			component="form"
+			onSubmit={formik.handleSubmit}
+			className="w-full flex items-center justify-center mt-[50px]" // centers child vertically and horizontally
+			padding={{ xs: "32px 16px", lg: "32px 56px 32px 152px" }}
+			sx={{ minHeight: "100vh" }} // ensures full viewport height for vertical centering
+		>
+			<CssBaseline />
+			<Box className="flex flex-col w-full gap-10 max-w-4xl">
+				<Box className="flex flex-col gap-4 max-w-[515px] w-full">
 					<Box
-						className="flex flex-col gap-3 justify-between overflow-auto"
-						height={{
-							xs: "calc(var(--window-height) - 368px)",
-							sm: "calc(var(--window-height) - 346px)",
-							md: "calc(var(--window-height) - 280px)",
+						className="flex items-center justify-center rounded-[10px] p-px w-min"
+						sx={{
+							background:
+								"linear-gradient(135deg, #6627FF 2.29%, #C9C0DF 44.53%, #131315 97.18%, #131315 97.18%)",
 						}}
 					>
-						<Box className="flex flex-col items-stretch gap-[40px] max-w-[552px] w-full">
-							<Box className="flex flex-col gap-[24px]">
-								<Box className="flex flex-col gap-[8px] max-w-[515px]">
-									<Typography color="#ABA9B1" fontSize="14px" fontWeight="400" lineHeight="20px">
-										Username
-									</Typography>
-									<Box className="flex flex-col gap-[6px]">
-										<Input
-											fullWidth
-											id="username"
-											name="username"
-											type="text"
-											placeholder="Enter username"
-											variant="outlined"
-											value={formik.values.username}
-											onChange={formik.handleChange}
-											onBlur={formik.handleBlur}
-											error={formik.touched.username && Boolean(formik.errors.username)}
-											helperText={formik.touched.username && formik.errors.username}
-										/>
-									</Box>
-								</Box>
-								<Box className="flex flex-col gap-[8px] max-w-[515px]">
-									<Typography color="#ABA9B1" fontSize="14px" fontWeight="400" lineHeight="20px">
-										Password
-									</Typography>
-									<Box className="flex flex-col gap-[6px]"></Box>
+						<Typography className="flex items-center justify-center size-10 rounded-[9px] font-manrope font-semibold text-xl leading-[22px] bg-black">
+							👋
+						</Typography>
+					</Box>
+					<Typography fontSize="24px" fontWeight={600} lineHeight="22px" color="#FFF">
+						Login to your account
+					</Typography>
+					<Typography fontSize="16px" fontWeight={500} lineHeight="22px" color="#ADADAD">
+						Welcome back! Please enter your details.
+					</Typography>
+				</Box>
+				<Box
+					className="flex flex-col gap-3 justify-between overflow-auto"
+					height={{
+						xs: "calc(var(--window-height) - 368px)",
+						sm: "calc(var(--window-height) - 346px)",
+						md: "calc(var(--window-height) - 280px)",
+					}}
+				>
+					<Box className="flex flex-col items-stretch gap-[40px] max-w-[552px] w-full">
+						<Box className="flex flex-col gap-[24px]">
+							<Box className="flex flex-col gap-[8px] max-w-[515px]">
+								<Typography color="#ABA9B1" fontSize="14px" fontWeight="400" lineHeight="20px">
+									Username
+								</Typography>
+								<Box className="flex flex-col gap-[6px]">
 									<Input
 										fullWidth
-										id="password"
-										name="password"
-										type={showPassword ? "text" : "password"}
-										placeholder="Enter password"
+										id="username"
+										name="username"
+										type="text"
+										placeholder="Enter username"
 										variant="outlined"
-										value={formik.values.password}
+										value={formik.values.username}
 										onChange={formik.handleChange}
 										onBlur={formik.handleBlur}
-										error={formik.touched.password && Boolean(formik.errors.password)}
-										helperText={formik.touched.password && formik.errors.password}
-										InputProps={{
-											endAdornment: (
-												<InputAdornment position="end">
-													<IconButton
-														aria-label="toggle password visibility"
-														onClick={() => setShowPassword(!showPassword)}
-														onMouseDown={(event) => event.preventDefault()}
-														edge="end"
-													>
-														{showPassword ? (
-															<Eye size="18px" color="#FFF" />
-														) : (
-															<EyeSlash size="18px" color="#FFF" />
-														)}
-													</IconButton>
-												</InputAdornment>
-											),
-										}}
+										error={formik.touched.username && Boolean(formik.errors.username)}
+										helperText={formik.touched.username && formik.errors.username}
 									/>
 								</Box>
 							</Box>
-							<Box className="flex flex-col gap-[24px]">
-								<Box className="flex flex-col gap-[8px] max-w-[515px]">
-									<Button
-										type="submit"
-										color="primary"
-										className="bg-white w-full text-[#0A0A0A]"
-									>
-										Login <ArrowRight color="#0A0A0A" size={16} />
-									</Button>
-								</Box>
+							<Box className="flex flex-col gap-[8px] max-w-[515px]">
+								<Typography color="#ABA9B1" fontSize="14px" fontWeight="400" lineHeight="20px">
+									Password
+								</Typography>
+								<Box className="flex flex-col gap-[6px]"></Box>
+								<Input
+									fullWidth
+									id="password"
+									name="password"
+									type={showPassword ? "text" : "password"}
+									placeholder="Enter password"
+									variant="outlined"
+									value={formik.values.password}
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									error={formik.touched.password && Boolean(formik.errors.password)}
+									helperText={formik.touched.password && formik.errors.password}
+									InputProps={{
+										endAdornment: (
+											<InputAdornment position="end">
+												<IconButton
+													aria-label="toggle password visibility"
+													onClick={() => setShowPassword(!showPassword)}
+													onMouseDown={(event) => event.preventDefault()}
+													edge="end"
+												>
+													{showPassword ? (
+														<Eye size="18px" color="#FFF" />
+													) : (
+														<EyeSlash size="18px" color="#FFF" />
+													)}
+												</IconButton>
+											</InputAdornment>
+										),
+									}}
+								/>
+							</Box>
+						</Box>
+						<Box className="flex flex-col gap-[24px]">
+							<Box className="flex flex-col gap-[8px] max-w-[515px]">
+								<Button type="submit" color="primary" className="bg-white w-full text-[#0A0A0A]">
+									Login <ArrowRight color="#0A0A0A" size={16} />
+								</Button>
 							</Box>
 						</Box>
 					</Box>
