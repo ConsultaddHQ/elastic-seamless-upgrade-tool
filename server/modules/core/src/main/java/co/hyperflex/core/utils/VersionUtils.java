ed713e457cdbac7aa62090f4a1b2015f0d1014f3
srajansohani <99042007+srajansohani@users.noreply.github.com>
2026-02-18 20:16:10 +0530

    UPDATED: Consider release date in upgrade path for out of order releases

--- server/modules/core/src/main/java/co/hyperflex/core/utils/VersionUtils.java
+++ server/modules/core/src/main/java/co/hyperflex/core/utils/VersionUtils.java
@@ -1,6 +1,8 @@
 package co.hyperflex.core.utils;
 
+import java.time.LocalDate;
 import java.util.Comparator;
+import org.springframework.data.util.Pair;
 
 public class VersionUtils {
   public static Comparator<String> VERSION_COMPARATOR = (v1, v2) -> {
@@ -27,13 +29,19 @@ public class VersionUtils {
     return VersionUtils.VERSION_COMPARATOR.compare(version2, version1) < 0;
   }
 
+  //Is version Greater considering out of order
+  public static boolean isVersionGtConsideringOOO(Pair<String, LocalDate> version1, Pair<String, LocalDate> version2) {
+    return VersionUtils.VERSION_COMPARATOR.compare(version2.getFirst(), version1.getFirst()) < 0
+        && (version1.getSecond().isBefore(version2.getSecond())
+        || version1.getSecond().isEqual(version2.getSecond()));
+  }
+
   public static boolean isMajorVersionUpgrade(String currentVersion, String targetVersion) {
     return currentVersion.charAt(0) != targetVersion.charAt(0);
   }
 
   public static boolean isValidUpgrade(String currentVersion, String targetVersion) {
-    boolean isCompatibleMajorVersionChange = (currentVersion.equals("7.17.28"))
-        || (currentVersion.equals("8.19.0"));
+    boolean isCompatibleMajorVersionChange = (currentVersion.equals("7.17.28")) || (currentVersion.equals("8.19.0"));
     int currentMajorVersion = Integer.parseInt(currentVersion.substring(0, currentVersion.indexOf('.')));
     int targetMajorVersion = Integer.parseInt(targetVersion.substring(0, targetVersion.indexOf('.')));
     return (currentMajorVersion == targetMajorVersion
