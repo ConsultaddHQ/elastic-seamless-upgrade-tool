4b747d427134568609de0c62f56501dc16943ff5
Nilesh Morkar <nilesh.m@consultadd.com>
2026-04-06 17:01:25 +0530

    Fix kibana upgrade issue for 7.17 to 8.x upgrade.

--- server/modules/upgrade/src/main/java/co/hyperflex/upgrade/tasks/kibana/BackupKibanaServiceFileTask.java
+++ server/modules/upgrade/src/main/java/co/hyperflex/upgrade/tasks/kibana/BackupKibanaServiceFileTask.java
@@ -24,14 +24,14 @@ public class BackupKibanaServiceFileTask extends AbstractAnsibleTask {
     logger.info("Imp: If present, it will be safely renamed to prevent systemd conflicts during the 8.x boot sequence.");
 
     String backupScript =
-        "if [ -f /etc/systemd/system/kibana.service ]; then " +
-            "echo 'Legacy override file found. Backing up to .7x_backup...'; " +
-            "mv /etc/systemd/system/kibana.service /etc/systemd/system/kibana.service.7x_backup; " +
-            "systemctl daemon-reload; " +
-            "echo 'Backup complete and systemd daemon reloaded.'; " +
-            "else " +
-            "echo 'No legacy override file found. Skipping backup.'; " +
-            "fi";
+        "if [ -f /etc/systemd/system/kibana.service ]; then "
+            + "echo 'Legacy override file found. Backing up to .7x_backup...'; "
+            + "mv /etc/systemd/system/kibana.service /etc/systemd/system/kibana.service.7x_backup; "
+            + "systemctl daemon-reload; "
+            + "echo 'Backup complete and systemd daemon reloaded.'; "
+            + "else "
+            + "echo 'No legacy override file found. Skipping backup.'; "
+            + "fi";
 
     AnsibleAdHocCommand command = AnsibleAdHocCommand.builder()
         .module("shell")
